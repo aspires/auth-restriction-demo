e901578016bb5cca9879b2468ebc4af675d3244a
Austin Spires <austinspires@gmail.com>
2012-04-20 22:32:15 -0500

    Added Styling, Front end Work, REstrictions Secure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  def index
-  end
+  before_filter :authenticate_user!
+  load_and_authorize_resource only: :index
 
   def show
+    @user = User.find(params[:id])
   end
 end
